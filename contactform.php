2c494fbeaf7991ecddcfd008ea753fd0c49b4c86
Stephanie Castaneda <stephaniec1997@yahoo.com>
2019-09-20 20:17:31 -0700

    last time

--- contactform.php
+++ contactform.php
@@ -1,18 +1,17 @@
 <?php
 
-if (isset($_POST['submit'])){
-  $name = $_POST['name'];
-  $mailFrom = $_POST['mail'];
-  $message = $_POST['message'];
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$mailFrom = $_POST['mail'];
+$message = $_POST['message'];
 
-  $subject = "New message from your Profile page.";
-  $mailTo = "stephanie_cast@yahoo.com";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
+$subject = "New Message from profile page!";
+$mailTo = "stephanie_cast@yahoo.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-  mail($mailTo, $subject, $txt, $headers);
-
-  header("Location: index.php?mailsend");
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
 }
 
 ?>
