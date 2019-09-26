154263e8d622e27d92b17f8b5a9d312b111b2cad
Stephanie Castaneda <stephaniec1997@yahoo.com>
2019-09-25 17:27:26 -0700

    Changed the content of page

---
+++ index.js
@@ -0,0 +1,8 @@
+function showHide() {
+  var x = document.getElementById("FloodIt");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
